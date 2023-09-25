449e497bc831c5e263f6c0b6e1b6b6a91093d2b7
Ehsan Foroumandi <49204626+ehsanfrmd@users.noreply.github.com>
2023-09-25 10:59:21 -0500

    Create simpleGPU.cu

---
+++ simpleGPU.cu
@@ -0,0 +1,117 @@
+/*
+ Name: Ehsan Foroumandi
+*/
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <sys/time.h>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::generate;
+using std::vector;
+double gettime(void) {
+		struct timeval tval;
+
+		  gettimeofday(&tval, NULL);
+
+  	return( (double)tval.tv_sec + (double)tval.tv_usec/1000000.0 );
+}
+
+__global__ void matrixMul(const int *a, const int *b, int *c, int N) {
+  // Compute each thread's global row and column index
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  // Iterate over row, and down column
+  c[row * N + col] = 0;
+  for (int k = 0; k < N; k++) {
+    // Accumulate results for a single element
+    c[row * N + col] += a[row * N + k] * b[k * N + col];
+  }
+}
+
+// Check result on the CPU
+void verify_result(vector<int> &a, vector<int> &b, vector<int> &c, int N) {
+  // For every row...
+  for (int i = 0; i < N; i++) {
+    // For every column...
+    for (int j = 0; j < N; j++) {
+      // For every element in the row-column pair
+      int tmp = 0;
+      for (int k = 0; k < N; k++) {
+        // Accumulate the partial results
+        tmp += a[i * N + k] * b[k * N + j];
+      }
+
+      // Check against the CPU result
+      assert(tmp == c[i * N + j]);
+    }
+  }
+}
+
+int main() {
+  
+  int N = 10000;
+  double t1, t2, t1_cpu, t2_cpu;
+	t1 = gettime();
+
+  // Size (in bytes) of matrix
+  size_t bytes = N * N * sizeof(int);
+
+  // Host vectors
+  vector<int> h_a(N * N);
+  vector<int> h_b(N * N);
+  vector<int> h_c(N * N);
+
+  // Initialize matrices
+  generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+  generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
+
+  // Allocate device memory
+  int *d_a, *d_b, *d_c;
+  cudaMalloc(&d_a, bytes);
+  cudaMalloc(&d_b, bytes);
+  cudaMalloc(&d_c, bytes);
+
+  // Copy data to the device
+  cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
+
+  // Threads per CTA dimension
+  int THREADS = 20;
+
+  // Blocks per grid dimension (assumes THREADS divides N evenly)
+  int BLOCKS = N / THREADS;
+
+  // Use dim3 structs for block  and grid dimensions
+  dim3 threads(THREADS, THREADS);
+  dim3 blocks(BLOCKS, BLOCKS);
+
+  // Launch kernel
+  matrixMul<<<blocks, threads>>>(d_a, d_b, d_c, N);
+
+  // Copy back to the host
+  cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
+  t2 = gettime();
+  printf("Run time for the gpu: %f\n", t2-t1);
+
+  // Check result
+  t1_cpu = gettime();
+  verify_result(h_a, h_b, h_c, N);
+  t2_cpu = gettime();
+  printf("Run time for the cpu: %f\n", t2_cpu-t1_cpu);
+  cout << "COMPLETED SUCCESSFULLY\n";
+
+  // Free memory on device
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+  t2 = gettime();
+	printf("Run time: %f\n", t2-t1);
+
+  return 0;
+}
